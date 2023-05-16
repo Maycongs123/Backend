3ec219a96587ea7c69ba5d046f76563afd0bf59f
maycon.santos <maycongs123@gmail.com>
2023-05-16 11:06:22 -0300

    first commit

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+//DB Connection
+
+const conn = require("./db/conn");
+
+conn();
+
+app.listen(3001, function(){
+    console.log("Servidor Online!!")
+});
+
+
+
+
